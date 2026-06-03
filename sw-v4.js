a7f54fd97ceba617c9321be1f3c7978ed00631eb
ambook <abby@abbybookdeMacBook-Air.local>
2026-06-03 20:20:13 +0800

    补回404+sw-v4

---
+++ sw-v4.js
@@ -0,0 +1,12 @@
+const CACHE = 'sudoku-v4';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
